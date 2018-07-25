756f9c84151b50586a4109c5cef5a42a56eb4953
chenkj <1097823199@qq.com>
2018-07-25 15:52:34 +0800

    redis session

--- src/main/java/com/chenkj/model/User.java
+++ src/main/java/com/chenkj/model/User.java
@@ -1,6 +1,9 @@
 package com.chenkj.model;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
 	private String userid;
 	private String password;
 	private String name;
--- src/test/java/com/chenkj/JunitTest.java
+++ src/test/java/com/chenkj/JunitTest.java
@@ -3,24 +3,25 @@ package com.chenkj;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.chenkj.ehcache.EhcacheService;
+import com.chenkj.model.User;
+import com.chenkj.util.RedisUtil;
+
 @ContextConfiguration(locations = { "classpath:applicationContext.xml" })  
 @RunWith(SpringJUnit4ClassRunner.class)  
 public class JunitTest extends AbstractJUnit4SpringContextTests {
 	@Autowired 
-	private EhcacheService ehcacheService;
+	private RedisTemplate<String,Object> redisTemplate;
 	
 	@Test
     public void testTimestamp() throws InterruptedException{
-		System.out.println("第一次调用：" + ehcacheService.getTimestamp("param"));
-        Thread.sleep(2000);
-        System.out.println("2秒之后调用：" + ehcacheService.getTimestamp("param"));
-        Thread.sleep(11000);
-        System.out.println("再过11秒之后调用：" + ehcacheService.getTimestamp("param"));
+		RedisUtil redis = new RedisUtil();
+		redis.setRedisTemplate(redisTemplate);
+		redis.set("name", new User("22","33"));
     }
 	
 }
